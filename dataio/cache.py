c4d93f7cc49ab2f511a4a0790d7ff7565efbdd8b
cem5113 <cem5113@hotmail.com>
2025-10-12 12:00:34 +0300

    Delete dataio/cache.py

--- dataio/cache.py
+++
@@ -1,8 +0,0 @@
-# dataio/cache.py
-from __future__ import annotations
-from functools import lru_cache
-
-# basit kullanım:
-# @disk_or_mem_cache(...) -> burada sadece bellek içi örnek veriyoruz
-def memcache(maxsize: int = 8):
-    return lru_cache(maxsize=maxsize)
