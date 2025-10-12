0069365f4cc8349844782eff49ace37295c7918d
cem5113 <cem5113@hotmail.com>
2025-10-12 12:00:02 +0300

    Delete features directory

--- features/spatial_grid.py
+++
@@ -1,7 +0,0 @@
-# features/spatial_grid.py
-from __future__ import annotations
-from typing import List
-
-def geoid_neighbors(geoid: str) -> List[str]:
-    # Geometri yoksa boş dön (ileride queen/rook komşuluk eklenecek)
-    return []
